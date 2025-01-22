e1b09ff7b3f86821629f6ea14ced4240a04d4c3f
Nidhish <nidhish.shastri1234@gmail.com>
2025-01-22 21:08:08 +0530

    updated for CORS

--- src/main/java/com/example/api2/config/WebConfig.java
+++ src/main/java/com/example/api2/config/WebConfig.java
@@ -1,4 +1,5 @@
-package config;
+package com.example.api2.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // Change to your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
